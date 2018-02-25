813287815089aa114956eadc95df3f18c79b13a5
mostafi <p.mostafij@gmail.com>
2018-02-25 01:46:47 -0800

    master cleanup for annotation

--- src/test/java/com/mlcode/test/demo1/BeanScopeDemoApp.java
+++ src/test/java/com/mlcode/test/demo1/BeanScopeDemoApp.java
@@ -7,24 +7,24 @@ public class BeanScopeDemoApp {
 
     public static void main(String[] args) {
 
-        //Load the spring configuration file
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
-
-        //retrieve bean from spring container
-        Coach footBallCoachCoach = applicationContext.getBean("footBallCoach", Coach.class);
-
-        Coach alphaFootBallCoach = applicationContext.getBean("footBallCoach", Coach.class);
-
-        System.out.println("Are they same object: " + (footBallCoachCoach == alphaFootBallCoach));
-        System.out.println("Memory Reference of footBallCoachCoach: " + footBallCoachCoach);
-        System.out.println("Memory Reference of alphaFootBallCoach: " + alphaFootBallCoach);
-
-        Coach trackCoach = applicationContext.getBean("trackCoach", Coach.class);
-
-        System.out.println(trackCoach.dailyWorkout());
-
-
-        applicationContext.close();
+//        //Load the spring configuration file
+//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+//
+//        //retrieve bean from spring container
+//        Coach footBallCoachCoach = applicationContext.getBean("footBallCoach", Coach.class);
+//
+//        Coach alphaFootBallCoach = applicationContext.getBean("footBallCoach", Coach.class);
+//
+//        System.out.println("Are they same object: " + (footBallCoachCoach == alphaFootBallCoach));
+//        System.out.println("Memory Reference of footBallCoachCoach: " + footBallCoachCoach);
+//        System.out.println("Memory Reference of alphaFootBallCoach: " + alphaFootBallCoach);
+//
+//        Coach trackCoach = applicationContext.getBean("trackCoach", Coach.class);
+//
+//        System.out.println(trackCoach.dailyWorkout());
+//
+//
+//        applicationContext.close();
     }
 
 }
--- src/test/java/com/mlcode/test/demo1/HelloSpringTest.java
+++ src/test/java/com/mlcode/test/demo1/HelloSpringTest.java
@@ -7,26 +7,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloSpringTest {
 
     public static void main(String[] args){
-        //load the spring configuration file
-        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-        //retrieve bean from spring container
-        Coach coach1 = applicationContext.getBean("footBallCoach", Coach.class);
-        Coach coach2 = applicationContext.getBean("trackCoach", Coach.class);
-        CricketCoach coach3 = applicationContext.getBean("cricketCoach", CricketCoach.class);
-
-        //call methods on the bean
-        System.out.println(coach1.dailyWorkout());
-        System.out.println(coach1.getDailyFortune());
-        System.out.println(coach2.dailyWorkout());
-        System.out.println(coach2.getDailyFortune());
-
-        System.out.println(coach3.dailyWorkout());
-        System.out.println(coach3.getDailyFortune());
-        System.out.println(coach3.getEmailAddress());
-        System.out.println(coach3.getTeam());
-
-        //close the context
-        applicationContext.close();
+//        //load the spring configuration file
+//        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//        //retrieve bean from spring container
+//        Coach coach1 = applicationContext.getBean("footBallCoach", Coach.class);
+//        Coach coach2 = applicationContext.getBean("trackCoach", Coach.class);
+//        CricketCoach coach3 = applicationContext.getBean("cricketCoach", CricketCoach.class);
+//
+//        //call methods on the bean
+//        System.out.println(coach1.dailyWorkout());
+//        System.out.println(coach1.getDailyFortune());
+//        System.out.println(coach2.dailyWorkout());
+//        System.out.println(coach2.getDailyFortune());
+//
+//        System.out.println(coach3.dailyWorkout());
+//        System.out.println(coach3.getDailyFortune());
+//        System.out.println(coach3.getEmailAddress());
+//        System.out.println(coach3.getTeam());
+//
+//        //close the context
+//        applicationContext.close();
     }
 }
